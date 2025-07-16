5601ecdf51866e5798237f575008a2c4c0e363de
ivanbardon <elsegundopar@gmail.com>
2025-07-16 05:29:05 +0200

    first commit

---
+++ webxavi/xavi_react/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold'>Welcome to Xavi React App</h1>
+    </>
+  )
+}
+
+export default App
